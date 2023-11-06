3079a276c83585994d51408286d2000015a04f40
caiofariaas <caiofarias077@gmail.com>
2023-11-06 09:15:35 -0300

    new

--- Func.py
+++
@@ -1,8 +0,0 @@
-from Jogador import Jogador
-from Dealer import Dealer
-
-def prntVencedor():
-    print("-=" * 18)
-    print(f"\n{dealer.vencedor(jogadores, apostas)} !")
-    print("-=" * 18)
-    exit()
